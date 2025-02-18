b86af2ced42ece6d18abfef66f6f20da1f3619c4
Holedesu <88774183+Holedesu@users.noreply.github.com>
2025-02-18 14:25:31 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from asgiref.sync import sync_to_async
+import django
+import os
+import random
+
+
+
+# if __name__ == "__main__":
+#     application = (
+#         ApplicationBuilder()
+#         .token("INSERT_YOUR_TOKEN_INSTEAD_OF_THIS_TEXT")
+#         .build()
+#     )
